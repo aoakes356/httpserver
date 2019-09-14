a426948cba6af57070dc0974d24800fe73bb0348
= <=>
2019-09-13 23:15:59 -0700

    minor tweak

--- client.py
+++ client.py
@@ -3,5 +3,5 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(),8000))
 
-recieved = s.recv(1024)
-print(str(recieved))
+recieved = (s.recv(1024)).decode("utf-8")
+print(recieved)
