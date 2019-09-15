1aad429bd20746d6479fa62e12ebe20878b008ed
= <=>
2019-09-14 17:15:10 -0700

    Can now connect a bunch of clients, will process input from them all.

--- server.py
+++ server.py
@@ -1,10 +1,55 @@
 import socket
+import select
+import queue
+import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #IPv4, TCP 
-s.bind((socket.gethostname(),8000))
+s.bind(('localhost',8000))
 s.listen(500)
+reading = [s]
+writing = []
+outgoing = {}   # dictionary of queues, mapped to by their sockets.
+waiting = {}    # dictionary of times mapped to by their sockets.
+def buffered_read(sock):
+    buff = ""
+    delta = "ayy"
+    while delta != "":
+        delta = sock.recv(1024).decode("utf-8")
+        buff += delta
+        if(len(delta) < 1024):
+            break
+    return buff
 
 while True:
-    clientSocket, address = s.accept()
-    print(f"Connection from {address} has been established.")
-    clientSocket.send("Die in a fire".encode("utf-8"))
+    read, write, err = select.select(reading,writing,reading)
+    for opened in read:
+        if not (opened is s):
+            # A client is sending data
+            # Later we will want to parse the buffered read, and generate a response.
+            print("Waiting to read!")
+            read = buffered_read(opened)
+            if read:
+                print("Read something!")
+                print("Read from the client: ",read)
+                outgoing[opened].put(read)# This echos
+                if not (opened in writing): 
+                    writing.append(opened)
+            else:
+                print("Didn't read anything!")
+                # Nothing from that client
+                opened.close()    
+                if opened in writing:
+                    writing.remove(opened)
+                reading.remove(opened)
+                del outgoing[opened]
+        else:
+            # New connection!
+            print("Waiting on connection!")
+            clientSocket, address = opened.accept()
+            clientSocket.setblocking(0)
+            outgoing[clientSocket] = queue.Queue()  # Give it a response queue.
+            print(f"Connection from {address} has been established.")
+            reading.append(clientSocket);   #Add the client socket for later reading.
+    for writeable in write:
+        outgoing[writeable]
+
